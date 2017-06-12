94bd0114ab2d4887453d37114650136bfd44c48f
saumil shah <SaumilShah66@users.noreply.github.com>
2017-06-12 18:26:18 +0530

    Create apply.py

--- apply.py
+++ apply.py
@@ -6,6 +6,8 @@ from tflearn.layers.core import input_data, dropout, fully_connected
 from tflearn.layers.conv import conv_2d, max_pool_2d
 from tflearn.layers.estimator import regression
 import cv2
+
+image_name = '/cats/cat.11737.jpg'
 network = input_data(shape=[None, 50, 50, 1])
 
 network = conv_2d(network, 30, 3, activation='relu')
@@ -40,7 +42,7 @@ model = tflearn.DNN(network)
 
 print('Model has been created')
 print('Loading an image')
-img = cv2.imread('cat.jpg',0)
+img = cv2.imread(image_name,0)
 img = cv2.resize(img,(50,50))
 
 print('Loading the network')
@@ -49,6 +51,11 @@ print('Network loaded')
 
 print('Making prediction')
 img = np.reshape(img,(-1,50,50,1))
-print(model.predict(img))
+predict = model.predict(img)
 
-
+if predict[0]>0.5:
+  print("It's a Dog")
+elif predict[1]>0.5:
+  print("It's a Cat")
+else:
+  print("I cannot define it")
