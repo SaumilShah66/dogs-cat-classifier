dbc0f8517d746554c89910f549bba59dc6908dc6
saumil shah <SaumilShah66@users.noreply.github.com>
2017-06-16 13:27:09 +0530

    Update apply.py

--- apply.py
+++ apply.py
@@ -7,7 +7,8 @@ from tflearn.layers.conv import conv_2d, max_pool_2d
 from tflearn.layers.estimator import regression
 import cv2
 
-image_name = 'cats/cat.11737.jpg'
+#Creating and loading trained model
+#---------------------------------------------------------
 network = input_data(shape=[None, 50, 50, 1])
 
 network = conv_2d(network, 30, 3, activation='relu')
@@ -39,18 +40,24 @@ network = fully_connected(network, 2, activation='softmax')
 network = regression(network)
 
 model = tflearn.DNN(network)
-
 print('Model has been created')
-print('Loading an image')
-img = cv2.imread(image_name,0)
-img = cv2.resize(img,(50,50))
-
 print('Loading the network')
 model.load('my_cnn.tflearn')
 print('Network loaded')
+#-------------------------------------------------------
+#Loading image
+print('Loading an image')
+image_name = 'cats/cat.11737.jpg'
+img = cv2.imread(image_name,0)
+img = cv2.resize(img,(50,50))
 
+# Making prediction
 print('Making prediction')
 img = np.reshape(img,(-1,50,50,1))
 predict = model.predict(img)
-
-print(predict)
+if pre[0][0] >= 0.5:
+    print("It's a Dog")
+elif pre[0][1] >= 0.5:
+    print("It's a Cat")
+else:
+    print("I can't predict")
